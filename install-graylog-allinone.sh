e368bd0440c94d961547dbec08350e172ea26bf8
tangomega <161986284+tangomega@users.noreply.github.com>
2025-09-03 15:07:46 -0500

    Update install-graylog-allinone.sh

--- install-graylog-allinone.sh
+++ install-graylog-allinone.sh
@@ -47,7 +47,7 @@ detect_ubuntu_codename() {
 install_mongodb() {
   echo "--- Installing MongoDB 7.0 ---"
   apt-get install -y gnupg curl || true
-  curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg --dearmor -o /usr/share/keyrings/mongodb-server-7.0.gpg
+  curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg --dearmor -o /usr/share/keyrings/mongodb-server-8.0.gpg
   # Use detected codename (jammy/focal)
   echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
   apt-get update -y
