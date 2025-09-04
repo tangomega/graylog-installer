cb08b6eba1a67d52f78f1cbf51ec3bb61304a3b9
tangomega <161986284+tangomega@users.noreply.github.com>
2025-09-04 11:21:04 -0500

    Update installer.sh 4

--- installer.sh
+++ installer.sh
@@ -293,7 +293,7 @@ main() {
 
   # MongoDB (cite: https://www.mongodb.org/docs/manual/tutorial/install-mongodb-on-ubuntu/)
   if ! dpkg -s mongodb-org &>/dev/null; then
-    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | gpg --dearmor -o /usr/share/keyrings/mongodb-server Cristián Ramírez Rodríguez (CRo) -8.0.gpg
+    curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
     echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-8.0.list
     apt-get update -y
     apt-get install -y mongodb-org
