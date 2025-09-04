8190124f33a8f542a4fc556c937e78b7a6db62b7
tangomega <161986284+tangomega@users.noreply.github.com>
2025-09-04 16:13:38 -0500

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,11 +7,10 @@ main() {
 
   sudo apt-get install graylog-server
   echo -n "Enter Password: " && head -1 </dev/stdin | tr -d '\n' | sha256sum | cut -d" " -f1
-  sed -i "/password_secret/c$(sed -n '/password_secret/{p;q}' /etc/graylog/datanode/datanode.conf)" /etc/graylog/server/server.conf
-  sed -n "http_bind_address
-
-sed -n '/PATTERN/{p;q}' source.txt >> destination.txt
+  sudo sed -i "/password_secret/c$(sed -n '/password_secret/{p;q}' /etc/graylog/datanode/datanode.conf)" /etc/graylog/server/server.conf
+  sudo sed -i '0,/http_bind_address/{s|.*http_bind_address.*|http_bind_address = 0.0.0.0:9000|}' /etc/graylog/server/server.conf
   
+
 }
 
 main "$@"
