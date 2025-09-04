bf2b64db128a8bf2d61263b52190197ba5b7f021
tangomega <161986284+tangomega@users.noreply.github.com>
2025-09-04 16:38:39 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -17,8 +17,6 @@ main() {
   sudo apt-get update
   sudo apt-get install -y mongodb-org
   sudo apt-mark hold mongodb-org
-
-  #Configure MongoDB
   sudo sed -i '/bindIp/c\  bindIpAll: true' /etc/mongod.conf
   sudo cat /etc/mongod.conf
  
@@ -31,19 +29,12 @@ main() {
   # Datanode
   echo "Starting Graylog DataNode installer"
 
-  #Remove old files
   sudo systemctl stop graylog-datanode && sudo apt purge --autoremove -y graylog-datanode && sudo rm -rf /etc/graylog/datanode /var/lib/graylog-datanode /var/log/graylog-datanode && sudo apt update
-
-  # Install essential packages
   sudo apt install -y gnupg curl wget apt-transport-https openssl ca-certificates jq openjdk-17-jre-headless
-
-  # Install Graylog-Datanode repo
   wget https://packages.graylog2.org/repo/packages/graylog-6.3-repository_latest.deb
   sudo dpkg -i graylog-6.3-repository_latest.deb
   sudo apt-get update
   sudo apt-get install graylog-datanode
-
-  #Configure Datanode
   echo 'vm.max_map_count=262144' | sudo tee -a /etc/sysctl.d/99-graylog-datanode.conf
   sudo sysctl --system
   cat /proc/sys/vm/max_map_count
@@ -55,6 +46,20 @@ main() {
   sudo systemctl enable graylog-datanode.service
   sudo systemctl start graylog-datanode
   sudo systemctl status graylog-datanode
+
+  #Graylog Server
+  echo "Starting Graylog Server installer"
+
+  sudo apt-get install graylog-server
+  sudo sed -i "/password_secret/c$(sed -n '/password_secret/{p;q}' /etc/graylog/datanode/datanode.conf)" /etc/graylog/server/server.conf
+  sudo sed -i '0,/http_bind_address/{s|.*http_bind_address.*|http_bind_address = 0.0.0.0:9000|}' /etc/graylog/server/server.conf
+  sudo read -sp "Enter Password: " pw && echo && hash=$(echo -n "$pw" | sha256sum | cut -d' ' -f1) && sudo sed -i "/^root_password_sha2 =/c\root_password_sha2 = $hash" /etc/graylog/server/server.conf
+  sudo sed -i '/^GRAYLOG_SERVER_JAVA_OPTS="-Xms1g/c\GRAYLOG_SERVER_JAVA_OPTS="-Xms2g -Xmx2g -server -XX:+UseG1GC -XX:-OmitStackTraceInFastThrow"' /etc/default/graylog-server
+  sudo systemctl daemon-reload
+  sudo systemctl enable graylog-server.service
+  sudo systemctl start graylog-server.service
+  sudo systemctl status graylog-server.service
+  tail /var/log/graylog-server/server.log
 }
 
 main "$@"
