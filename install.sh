3c49a791d97a999505337e500d781b4cf0a87d33
tangomega <161986284+tangomega@users.noreply.github.com>
2025-09-12 15:08:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ main() {
 
   # Install MongoDB
   curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
-  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
   sudo apt-get update
   sudo apt-get install -y mongodb-org
   sudo apt-mark hold mongodb-org
@@ -17,8 +17,11 @@ main() {
   # Configure MongoDB
   sudo sed -i '/bindIp/c\  bindIpAll: true' /etc/mongod.conf
   sudo systemctl daemon-reload
+  sleep 2
   sudo systemctl enable mongod.service
+  sleep 2
   sudo systemctl start mongod.service
+  sleep 2
 
   echo "Starting Graylog DataNode installer"
 
@@ -39,8 +42,11 @@ main() {
   sudo sed -i "/mongodb_uri/c\\mongodb_uri = mongodb://127.0.0.1:27017/graylog" /etc/graylog/datanode/datanode.conf
   echo "opensearch_heap = 4g" >> /etc/graylog/datanode/datanode.conf
   sudo systemctl daemon-reload
+  sleep 2
   sudo systemctl enable graylog-datanode.service
+  sleep 2
   sudo systemctl start graylog-datanode
+  sleep 2
 
   echo "Starting Graylog Server installer"
 
@@ -50,9 +56,9 @@ main() {
   sudo read -sp "Enter Password: " pw && echo && hash=$(echo -n "$pw" | sha256sum | cut -d' ' -f1) && sudo sed -i "/^root_password_sha2 =/c\root_password_sha2 = $hash" /etc/graylog/server/server.conf
   sudo sed -i '/^GRAYLOG_SERVER_JAVA_OPTS="-Xms1g/c\GRAYLOG_SERVER_JAVA_OPTS="-Xms2g -Xmx2g -server -XX:+UseG1GC -XX:-OmitStackTraceInFastThrow"' /etc/default/graylog-server
   sudo systemctl daemon-reload
-  sleep 1
+  sleep 2
   sudo systemctl enable graylog-server.service
-  sleep 1
+  sleep 2
   sudo systemctl start graylog-server.service
   sleep 5
   tail /var/log/graylog-server/server.log
